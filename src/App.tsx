b0c15bdd85848ce71f773516c064651a3896eca9
cheiru94 <cheiru94@gmail.com>
2024-01-26 00:03:07 +0900

    feat : 라우팅 틀 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <>Router</>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>HomePage</h1>}></Route>
+      <Route path="/posts" element={<h1>게시글 리스트</h1>}></Route>
+      <Route path="/posts/:id" element={<h1>게시글 상세</h1>}></Route>
+      <Route path="/posts/edit/:id" element={<h1>게시글 수정</h1>}></Route>
+      <Route path="/profile" element={<h1>프로필</h1>}></Route>
+      <Route path="/profile/edit" element={<h1>프로필 수정</h1>}></Route>
+      <Route path="/search" element={<h1>해시태그 검색</h1>}></Route>
+      <Route path="/notifications" element={<h1>알림</h1>}></Route>
+      <Route path="/users/login" element={<h1>로그인</h1>}></Route>
+      <Route path="/users/signup" element={<h1>회원가입</h1>}></Route>
+      <Route path="*" element={<Navigate replace to="/" />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
